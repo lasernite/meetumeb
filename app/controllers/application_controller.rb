fef3abcddb2c70e166f772b241b86ee1d65f763d
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-04-18 11:45:23 -0500

    add community to users3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   before_filter :location
+  before_filter :community
   
   def location
     @city = request.location.city
     @state = request.location.state
   end
+  
+  def community
+     @community = @city+@state
+   end
   
 end
