cb2f8ad11986d1994f33cf48dbff0aefbc40ecce
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-04-20 13:53:44 -0500

    updated config for heroku

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ end
 
 module Meetumea
   class Application < Rails::Application
+    #While precompiling assets prevent initializing your application and connecting to the database
+    config.assets.initialize_on_precompile = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
