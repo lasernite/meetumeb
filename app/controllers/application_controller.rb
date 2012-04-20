76d64972439891a8f849d7c0e8fb2e550da0c682
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-04-20 13:50:50 -0500

    community readded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  before_filter :community
+  
+  def community
+    @city = request.location.city
+    @state = request.location.state
+    @community = @city+@state
+   end
   
 end
