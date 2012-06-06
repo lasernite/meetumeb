b2f2bce2f759628c5c1a82e72452190e59710509
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-06-05 23:32:44 -0500

    deleted comments controller content

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,2 @@
 class CommentsController < ApplicationController
- 
- def create
-  @micropost = Micropost.find(params[:micropost_id])
-  @comment = Comment.new(params[:comment])
-  @comment.micropost = @micropost
-  @comment.user = current_user
-    if @comment.save
-      flash[:success] = "Comment created!"
-    redirect_to @micropost
-  else
-    render 'shared/_comment_form'
-  end
-  end
-end
-
- 
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-  end
 end
