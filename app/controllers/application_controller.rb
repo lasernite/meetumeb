0c693e8a6e925606c7029558f6eb4ede6e934fbd
Laser Nite <lasernite@gmail.com>
2012-10-17 18:13:16 -0500

    redirect to www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :add_www_subdomain
   include SessionsHelper
   before_filter :community
+  
+  private
+    def add_www_subdomain
+      unless /^www/.match(request.host)
+        redirect_to("#{request.protocol}x.com#{request.request_uri}",
+                    :status => 301)
+      end
+      end
   
   def community
     @city = request.location.city
